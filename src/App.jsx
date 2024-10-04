3693201ea37280ed4b72a08889fb7aba4434387f
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2024-10-05 02:26:12 +0600

    Done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,127 @@
 import { useState } from "react";
 
-const Square = () => {
-  const [value, setValue] = useState(null);
-
-  const handleClick = () => {
-    setValue("X");
-  };
+const Square = ({ value, onSquareClick }) => {
   return (
     <button
       className="bg-gradient-to-br from-green-400 to-cyan-600 border border-gray-300 rounded-lg h-16 w-16 m-2 shadow-md hover:shadow-lg transform hover:scale-105 transition-all duration-200 ease-in-out text-white text-2xl font-bold"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
   );
 };
 
-const Board = () => {
+const Board = ({ xIsNext, squares, onPlay }) => {
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner : ${winner}`;
+  } else {
+    status = "Next Player : " + (xIsNext ? "X" : "O");
+  }
+
+  const handleClick = (i) => {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares);
+  };
+
   return (
     <>
-      <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+      <div className="text-2xl text-center">{status}</div>
+      <div className="flex items-center justify-center">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
 
-      <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex items-center justify-center">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
 
-      <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex items-center justify-center">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
 };
 
-export default Board;
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const currentSquares = history[currentMove];
+
+  const handlePlay = (nextSquares) => {
+    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  };
+
+  const jumpTo = (move) => {
+    setCurrentMove(move);
+    setXIsNext(move % 2 === 0);
+  };
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go To The Move : ${move}`;
+    } else {
+      description = `Go To Start The Game`;
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="flex items-center justify-center">
+      <div>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="bg-green-400"> </div>
+      <div className="flex items-center justify-center">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
